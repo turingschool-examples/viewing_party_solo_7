1c9864f16846637293a0d68a505b6b14b7597aa5
Sam Puttman <daputter2@gmail.com>
2024-03-20 19:27:37 -0400

    Add test for user discover page.

---
+++ spec/features/users/discover_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Users" do
+  it "displays options to discover movies" do
+    user = User.create!(name: "Test User", email: "test@example.com")
+
+    visit discover_user_path(user)
+
+    expect(page).to have_http_status(:success)
+    expect(page.body).to include("Discover Top Rated Movies")
+    expect(page.body).to include("Search by Movie Title")
+  end
+end
