2395cc7e854ea16b4f09bb08017a858568b931cd
Sam Puttman <daputter2@gmail.com>
2024-03-21 12:42:23 -0400

    Add tests for additional movieservice methods

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -21,5 +21,39 @@ RSpec.describe MovieService, type: :service do
       expect(response[:results].first).to have_key(:title)
     end
   end
+  
+  describe "movie details", :vcr do
+    it "fetches details for a specific movie" do
+      service = MovieService.new
+      response = service.movie_details(11)
 
+      expect(response).to have_key(:title)
+      expect(response).to have_key(:vote_average)
+      expect(response).to have_key(:runtime)
+      expect(response).to have_key(:genres)
+      expect(response).to have_key(:overview)
+    end
+  end
+
+  describe "movie reviews", :vcr do
+    it "fetches reviews for a specific movie" do
+      service = MovieService.new
+      response = service.movie_reviews(11)
+
+      expect(response[:results]).to be_an(Array)
+    end
+  end
+
+  describe "movie credits", :vcr do
+    it "fetches credits for a specific movie" do
+      service = MovieService.new
+      response = service.movie_credits(11)
+
+      expect(response[:cast]).to be_an(Array)
+      expect(response[:cast].any? { |actor| actor[:name] == "Mark Hamill" }).to be true
+      expect(response[:cast].any? { |actor| actor[:name] == "Carrie Fisher" }).to be true
+      expect(response[:cast].any? { |actor| actor[:name] == "Harrison Ford" }).to be true
+      expect(response[:cast].any? { |actor| actor[:name] == "Tom Hanks" }).to be false
+    end
+  end
 end
