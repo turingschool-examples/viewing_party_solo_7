209a4ec64c8e3aa0013daabddef32c15a8bfd04e
Sam Puttman <daputter2@gmail.com>
2024-03-22 11:42:57 -0400

    Add model validations to verify date and start time of viewing parties

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -1,8 +1,13 @@
 class ViewingParty < ApplicationRecord
-   has_many :user_parties
-   has_many :users, through: :user_parties
 
-   def find_host
-      users.where("user_parties.host = true").first
-   end
+  has_many :user_parties
+  has_many :users, through: :user_parties
+
+  validates :date, presence: true
+  validates :start_time, presence: true
+
+
+  def find_host
+    users.where("user_parties.host = true").first
+  end
 end
