68b9cd6b8aa0722389f1aac84ae958e9ed453adb
Sam Puttman <daputter2@gmail.com>
2024-03-22 12:23:41 -0400

    Create viewing party facade to handle logic, refactor viewing party model.

--- app/models/viewing_party.rb
+++ app/models/viewing_party.rb
@@ -3,9 +3,7 @@ class ViewingParty < ApplicationRecord
   has_many :user_parties
   has_many :users, through: :user_parties
 
-  validates :date, presence: true
-  validates :start_time, presence: true
-
+  validates_presence_of :date, :start_time
 
   def find_host
     users.where("user_parties.host = true").first
